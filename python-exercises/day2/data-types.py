4cf69bc89f5293fdc973203b2ff14b6e0cf86dd2
John Hudgins <john.hudgins@gmail.com>
2024-01-03 16:12:22 -0500

    Day 2 work progress

---
+++ python-exercises/day2/data-types.py
@@ -0,0 +1,24 @@
+#Data Types
+
+#Strings
+
+"Hello"[4]
+
+print("123"+"345")
+
+#Integers
+
+print(123 + 345)
+
+#Float
+
+flt1 = 3.14159
+
+print(flt1)
+
+#Boolean
+
+True
+False
+
+
---
+++ python-exercises/day2/hashloop.py
@@ -0,0 +1,7 @@
+str1 = " "
+str2 = "#"
+i=1
+
+while i < 10:
+    print(str1*(10-i)+str2*i)
+    i = i + 1
