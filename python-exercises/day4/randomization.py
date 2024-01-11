2c84ced1938160df15e1c7f06723c3616c7d7657
John Hudgins <john.hudgins@gmail.com>
2024-01-11 10:12:33 -0500

    Some lunch-time day 4 changes

---
+++ python-exercises/day4/randomization.py
@@ -0,0 +1,17 @@
+import random
+import my_module
+
+random_int = random.randint(0, 4)
+print(random_int)
+
+# print(my_module.pi)
+
+random_float = random.random()
+print(random_float)
+
+random_total = random_int + random_float
+
+print(random_total)
+
+
+
